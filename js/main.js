098c4fb321f588288f6c11c798dda3073e80167a
liuyuhaha <1969073502@qq.com>
2019-07-18 16:09:25 +0800

    基本完成，如果出错将会弹出提示

--- js/main.js
+++ js/main.js
@@ -23,6 +23,19 @@ $(function(){
     });
     
     $form.on('submit',function(e){
-        e.preventDefault(); 
+        //.preventDefault()方法阻止元素发生默认的行为
+        e.preventDefault();
+        // trigger() 方法触发被选元素的指定事件类型。为了当用户提交的时候不刷新
+        //并且将所有input元素自动blur，进行检测
+        $inputs.trigger("blur");
+        for(var i=0; i<inputs.length; i++){
+            var item =inputs[i];
+            //.validator.is_valid()用来检测的私有方法
+            var r=item.validator.is_valid();
+            if(!r){
+                alert("invalid");
+                return;
+            }
+        } 
     })
 });
